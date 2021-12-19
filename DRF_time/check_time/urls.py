434facd53e130b75f59b87c430b97098e11c2629
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-19 20:53:22 +0300

    Fix error 500 after bad request.

--- DRF_time/check_time/urls.py
+++ DRF_time/check_time/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .views import time_is_right
 
 urlpatterns = [
--- DRF_time/check_time/views.py
+++ DRF_time/check_time/views.py
@@ -1,5 +1,6 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status, exceptions
 from rest_framework.parsers import JSONParser
 from .serializers import CheckTimeSerializer
 from django.utils import timezone
@@ -8,11 +9,15 @@ from django.utils import timezone
 @csrf_exempt
 def time_is_right(request):
     if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CheckTimeSerializer(data=data)
-        if serializer.is_valid():
-            if serializer.data.get('time') == timezone.localtime().strftime("%H:%M"):
-                return JsonResponse({'result': 'True'})
-            else:
-                return JsonResponse({'result': 'False'})
-        return JsonResponse({'error': 'The time entered is not in the correct format'})
+        try:
+            data = JSONParser().parse(request)
+            serializer = CheckTimeSerializer(data=data)
+            if serializer.is_valid():
+                if serializer.data.get('time') == timezone.localtime().strftime("%H:%M"):
+                    return JsonResponse({'result': 'True'})
+                else:
+                    return JsonResponse({'result': 'False'})
+            return JsonResponse(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+        except exceptions.APIException:
+            return HttpResponseBadRequest("The server could not understand the request due to invalid syntax.")
