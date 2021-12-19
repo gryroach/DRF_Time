fb6f00f509a377aff5456a6c7336c87554768808
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-20 00:51:20 +0300

    Add view method fore calculating time in delta. Update serializers.py, urls.py !!! incorrect result where delta > 1h and localtime about 00:00

--- DRF_time/check_time/urls.py
+++ DRF_time/check_time/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import time_is_right
+from .views import time_is_right, time_plus_delta_is_right
 
 urlpatterns = [
     path('time-is-right/', time_is_right),
+    path('time-plus-delta-if-right/', time_plus_delta_is_right)
 ]
--- DRF_time/check_time/views.py
+++ DRF_time/check_time/views.py
@@ -2,8 +2,9 @@ from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status, exceptions
 from rest_framework.parsers import JSONParser
-from .serializers import CheckTimeSerializer
+from .serializers import CheckTimeSerializer, CheckTimePlusDeltaSerializer
 from django.utils import timezone
+from datetime import timedelta, time
 
 
 @csrf_exempt
@@ -14,6 +15,35 @@ def time_is_right(request):
             serializer = CheckTimeSerializer(data=data)
             if serializer.is_valid():
                 if serializer.data.get('time') == timezone.localtime().strftime("%H:%M"):
+                    return JsonResponse({'result': 'True'})
+                else:
+                    return JsonResponse({'result': 'False'})
+            return JsonResponse(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+        except exceptions.APIException:
+            return HttpResponseBadRequest("The server could not understand the request due to invalid syntax.")
+
+
+@csrf_exempt
+def time_plus_delta_is_right(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            serializer = CheckTimePlusDeltaSerializer(data=data)
+            if serializer.is_valid():
+                delta = timedelta(hours=int(serializer.data.get('delta').split(':')[0]),
+                                  minutes=int(serializer.data.get('delta').split(':')[1]))
+
+                print(delta)
+                print((timezone.localtime() - delta).time())
+                print((timezone.localtime() + delta).time())
+                print(time(hour=int(serializer.data.get('time').split(':')[0]), minute=int(serializer.data.get('time').split(':')[1])))
+                # incorrect result where delta > 1h and localtime about 00:00
+                if (timezone.localtime() - delta).time() <= \
+                        time(hour=int(serializer.data.get('time').split(':')[0]),
+                             minute=int(serializer.data.get('time').split(':')[1])) <= \
+                        (timezone.localtime() + delta).time():
+
                     return JsonResponse({'result': 'True'})
                 else:
                     return JsonResponse({'result': 'False'})
