b455ad4fe317f41d4119fa4b878e1247f45f5483
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-20 22:07:31 +0300

    Add api_root view and url. Rename url "time-plus-delta-if-right" to "time-plus-delta-is-right"

--- DRF_time/check_time/urls.py
+++ DRF_time/check_time/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import time_is_right, time_plus_delta_is_right
+from .views import time_is_right, time_plus_delta_is_right, api_root
 
 urlpatterns = [
-    path('time-is-right/', time_is_right),
-    path('time-plus-delta-if-right/', time_plus_delta_is_right)
+    path('time-is-right/', time_is_right, name='time-is-right'),
+    path('time-plus-delta-is-right/', time_plus_delta_is_right, name='time-plus-delta-is-right'),
+    path('', api_root),
 ]
--- DRF_time/check_time/views.py
+++ DRF_time/check_time/views.py
@@ -3,7 +3,10 @@ import datetime
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status, exceptions
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .serializers import CheckTimeSerializer, CheckTimePlusDeltaSerializer
 from django.utils import timezone
 from datetime import timedelta
@@ -24,6 +27,9 @@ def time_is_right(request):
                                 status=status.HTTP_400_BAD_REQUEST)
         except exceptions.APIException:
             return HttpResponseBadRequest("The server could not understand the request due to invalid syntax.")
+    else:
+        return HttpResponseBadRequest('The server only provides post-requests in json format. '
+                                      'Example: { "time": "12:00" }')
 
 
 @csrf_exempt
@@ -47,3 +53,14 @@ def time_plus_delta_is_right(request):
                                 status=status.HTTP_400_BAD_REQUEST)
         except exceptions.APIException:
             return HttpResponseBadRequest("The server could not understand the request due to invalid syntax.")
+    else:
+        return HttpResponseBadRequest('The server only provides post-requests in json format. '
+                                      'Example: { "delta": "01:30", "time": "12:00" }')
+
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'Checking time': reverse('time-is-right', request=request),
+        'Checking time in delta': reverse('time-plus-delta-is-right', request=request)
+    })
