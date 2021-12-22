218a997513d741912272d12c3627b18153d025a9
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-22 21:03:01 +0300

    Added remote_url variable declaration via .env

--- DRF_time/check_time/utils.py
+++ DRF_time/check_time/utils.py
@@ -1,10 +1,21 @@
 import datetime
+import json
+
 import requests
 from dateutil import parser
 from django.http import HttpResponseBadRequest
 
+from pathlib import Path
+import environ
+import os
+
+root = environ.Path(__file__) -3
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(root, '.env'))     # reading .env file
+
 # url world time API
-url = 'http://worldtimeapi.org/api/timezone/Europe/Moscow/'
+remote_url = env.str('REMOTE_TIME_URL')
 
 
 def download_time(path):
@@ -14,10 +25,14 @@ def download_time(path):
         return HttpResponseBadRequest("The time server is not responded.")
     except KeyError:
         return HttpResponseBadRequest("The time server address is not valid.")
+    except json.JSONDecodeError:
+        return HttpResponseBadRequest(f"Failed to get data from the server by {remote_url}.")
+    except requests.exceptions.MissingSchema:
+        return HttpResponseBadRequest(f"Invalid REMOTE_TIME_URL.")
 
 
 def check_time_by_global(serializer, delta=None):
-    response_datetime = download_time(url)
+    response_datetime = download_time(remote_url)
     if isinstance(response_datetime, datetime.datetime):
         if delta:
             delta = datetime.timedelta(hours=int(serializer.data.get('delta').split(':')[0]),
