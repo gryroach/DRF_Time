29fa2432b17a71d22b7759a73b2f6462ff8c7797
Sergey Gavrilenko <gryroach@gmail.com>
2021-12-22 21:18:12 +0300

    Added default delta via .env Change time_is_right Delete time_in_delta_is_right

--- DRF_time/check_time/urls.py
+++ DRF_time/check_time/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import time_is_right, time_plus_delta_is_right, api_root
+from .views import time_is_right, api_root
 
 urlpatterns = [
     path('time-is-right/', time_is_right, name='time-is-right'),
-    path('time-plus-delta-is-right/', time_plus_delta_is_right, name='time-plus-delta-is-right'),
     path('', api_root),
 ]
--- DRF_time/check_time/views.py
+++ DRF_time/check_time/views.py
@@ -6,7 +6,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from .serializers import CheckTimeSerializer, CheckTimePlusDeltaSerializer
-from .utils import check_time_by_global
+from .utils import check_time_by_global, env
 
 
 @csrf_exempt
@@ -17,38 +17,19 @@ def time_is_right(request):
         except exceptions.APIException:
             return HttpResponseBadRequest("The server could not understand the request due to invalid syntax.")
 
-        serializer = CheckTimeSerializer(data=data)
-        if serializer.is_valid():
-            if check_time_by_global(serializer):
-                return JsonResponse({'result': 'True'})
-            else:
-                return JsonResponse({'result': 'False'})
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        return HttpResponseBadRequest('The server only provides post-requests in json format. '
-                                      'Example: { "time": "12:00" }')
-
-
-@csrf_exempt
-def time_plus_delta_is_right(request):
-    if request.method == 'POST':
-        try:
-            data = JSONParser().parse(request)
-        except exceptions.APIException:
-            return HttpResponseBadRequest("The server could not understand the request due to invalid syntax.")
-
+        if 'delta' not in data:
+            data['delta'] = env.str('DELTA_TIME_DEFAULT')
         serializer = CheckTimePlusDeltaSerializer(data=data)
         if serializer.is_valid():
             return JsonResponse({'result': check_time_by_global(serializer, delta=True)})
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
         return HttpResponseBadRequest('The server only provides post-requests in json format. '
-                                      'Example: { "delta": "01:30", "time": "12:00" }')
+                                      'Example: { "time": "12:00", "delta": "01:30" }')
 
 
 @api_view(['GET'])
 def api_root(request):
     return Response({
-        'Checking time': reverse('time-is-right', request=request),
-        'Checking time in delta': reverse('time-plus-delta-is-right', request=request)
+        'Checking time in delta': reverse('time-is-right', request=request),
     })
